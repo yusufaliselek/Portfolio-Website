73170b8cb2d697fae62d3ba078fb9814cca14f22
Yusuf Ali Selek <yusufaliselek41@gmail.com>
2022-04-13 17:27:11 +0300

    Added drop-down in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,25 @@ const navMenu = () => {
 };
 
 navMenu();
+
+/* Drop Down Menu*/ 
+const dropDownMenu = () => {
+  const dropDown1 = document.querySelector(".drop-down-1");
+  const dropDown2 = document.querySelector(".drop-down-2");
+  const frondendDevSkills = document.querySelector(".frontend-developer-skills");
+  const mobileDevSkills = document.querySelector(".mobile-app-developer-skills");
+
+  dropDown1.addEventListener("click", () => {
+    frondendDevSkills.classList.toggle("drop-down-inactive");
+    /*dropDown1.setAttribute("data-icon","akar-icons:chevron-down");*/
+  });
+
+  dropDown2.addEventListener("click", () => {
+    mobileDevSkills.classList.toggle("drop-down-inactive");
+  });
+
+};
+
+dropDownMenu();
+
+
