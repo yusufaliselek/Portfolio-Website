3b20ecc46e44d309cebf8fefe2c3b38aa013d673
Yusuf Ali Selek <yusufaliselek41@gmail.com>
2022-04-13 16:29:50 +0300

    website made responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
-    // Progress bars
-    $(document).ready(function() {
-        $('.progress .progress-bar').css("width",
-                  function() {
-                      return $(this).attr("aria-valuenow") + "%";
-                  }
-          )
-      });
+/* Responsive Navbar */
+
+const navMenu = () => {
+  const navbarToggler = document.querySelector(".navbar-toggler");
+  const navList = document.querySelector(".nav-list");
+
+  navbarToggler.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    navbarToggler.classList.toggle("toggle");
+  });
+};
+
+navMenu();
