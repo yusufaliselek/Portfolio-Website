b59114b26a3155f34282c6d09786e792da7626e3
yusufaliselek <yusufaliselek41@gmail.com>
2023-07-20 19:58:22 +0300

    .

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,13 +6,16 @@ const Home = () => {
     return (
         <Content pageName="Home" content={
             <div className='w-full h-full flex justify-center items-center'>
-                <div className='shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-white bg-opacity-50 border-opacity-70 rounded-md max-w-xl flex flex-col justify-center px-10 py-5 gap-8'>
-                    <img src={homeImage} />
-                    <div className='font-medium text-center text-2xl font-sans'>
-                        Hi, I'm Yusuf. <br /> I'm a Software Developer.
-                    </div>
-                    <div className='text-center font-sans'>
-                        I'm currently learning ASP.Net Core and React. I have experience with C#, Javascript, Python, and Typescript.
+                <div className='shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] 
+                bg-white bg-opacity-50 border-opacity-70 rounded-md max-w-xl flex flex-col justify-center px-10 py-10 '>
+                    <img src={homeImage} style={{ height: "250px", opacity: "0.85" }} className='rounded-t-md' />
+                    <div className='flex flex-col gap-5 border-b border-r border-l border-gray-100 border-opacity-40 rounded-b-md p-[20px_5px_20px_5px]'>
+                        <div className='font-medium text-center text-2xl font-sans'>
+                            Hi, I'm Yusuf. <br /> I'm a Software Developer.
+                        </div>
+                        <div className='font-mono text-center'>
+                            I'm currently learning ASP.Net Core and React. <br /> I have experience with C#, Javascript, Python, and Typescript.
+                        </div>
                     </div>
                 </div>
             </div>
